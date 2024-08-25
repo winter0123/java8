d52dd1bbb3412843a1b3e7b932f8845fb9a047bc
winter0123 <cloudmist.j@gmail.com>
2024-08-25 19:01:50 +0900

    자바에서 제공하는 함수형 인터페이스

--- src/main/java/com/example/java8to17/javafunctional/Plus10Functional.java
+++ src/main/java/com/example/java8to17/javafunctional/Plus10Functional.java
@@ -2,6 +2,7 @@ package com.example.java8to17.javafunctional;
 
 import java.util.function.Function;
 
+//java8에서 제공하는 함수형 인터페이스를 class로 만들어 사용한 형태
 public class Plus10Functional implements Function<Integer, Integer> {
     @Override
     public Integer apply(Integer integer) {
